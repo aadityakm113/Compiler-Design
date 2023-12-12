cbae8944d88243c69b95de4031452ac8a61a711f
aadityakm113 <aadityakm113@gmail.com>
2023-12-12 11:40:02 +0530

    2nd lab

--- constnot.c
+++ constnot.c
@@ -10,35 +10,58 @@ int main(){
     if (isdigit(str[0]))
         state=1;
     else
-        state=5;
-    while(str[i]!='\0' && state!=5)
+        state=8;
+    while(str[i]!='\0' && state!=8)
     {
         switch (state)
         {
         case 1: if(isdigit(str[i]))
                 state=1;
             else if (str[i]=='.')
-                state=3;
+                state=4;
             else if (str[i]=='e')
                 state=2;
             else
-                state=5;
+                state=8;
             break;
 
-        case 2:if(isdigit(str[i]))
-                state=2;
+        case 2:if(isdigit(str[i])||str[i]=='+'||str[i]=='-')
+                state=3;
             else   
-                state=5; 
+                state=8; 
             break;
         case 3:if(isdigit(str[i]))
-                state=4;
+                state=3;
             else
-                state=5;
+                state=8;
             break;
         case 4:if(isdigit(str[i]))
-                state=4;
-            else
                 state=5;
+            else
+                state=8;
+            break;
+
+        case 5:if(isdigit(str[i]))
+                state=5;
+
+            else if (str[i]=='e')
+                state=6;
+            else
+                state=8;
+            break;
+
+        case 6:if(isdigit(str[i])||str[i]=='+'||str[i]=='-')
+                state=7;
+
+            else
+                state=8;
+            break;
+
+        case 7:if(isdigit(str[i]))
+                state=7;
+
+            else
+                state=8;
             break;
 
         }
@@ -49,9 +72,11 @@ int main(){
     printf("%s ",str);
     if(state==1)
         printf("is a Constant");
-    else if (state==2)
+    else if (state==3)
         printf("is a constant");
-    else if(state==4)
+    else if(state==5)
+        printf("is a Constant");
+    else if(state==7)
         printf("is a Constant");
     else
         printf("is not a Constant");
