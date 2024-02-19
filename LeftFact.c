d3451af78660320d62f893924b0543e06e65d09d
aadityakm113 <aadityakm113@gmail.com>
2024-02-19 21:49:19 +0530

    7&8

---
+++ LeftFact.c
@@ -0,0 +1,36 @@
+//Write a program that reads CFG and if required apply the left factoring then display the resulting CFG 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char gram[20],part1[20],part2[20],modifiedGram[20],newGram[20],tempGram[20];
+    int i,j=0,k=0,l=0,pos;
+    printf("Enter the production : A->");
+    scanf("%s",gram);
+
+    for (i=0;gram[i]!='|';i++,j++)
+        part1[i]=gram[i];
+    part1[j]='\0';
+    for(i=0;i<strlen(part1)||i<strlen(part2);i++)
+    {
+        if(part1[i]==part2[i])
+        {
+            modifiedGram[k]=part1[i];
+            k++;
+            pos=i+1;
+        }
+
+    }
+    for(i=pos,j=0;part1[i]!='\0';i++,j++){
+        newGram[j]=part1[i];
+    }
+    newGram[j++]='|';
+    for(i=pos;part2[i]!='\0';i++,j++){
+        newGram[j]=part2[i];
+    }
+    modifiedGram[k]='X';
+    modifiedGram[++k]='\0';
+    newGram[j]='\0';
+    printf("\n A->%s",modifiedGram);
+    printf("\n X->%s\n",newGram);
+}
